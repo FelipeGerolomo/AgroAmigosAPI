ab2e3c487b8d1f703e0be480ad731974551bd575
DESKTOP-LLU9OKL\FelipeGerolomo <felipegerolomo@gmail.com>
2018-11-26 07:57:34 -0200

    Config

--- index.js
+++ index.js
@@ -15,11 +15,24 @@ const tokenList = {};
 
 app.use(bodyParser.json());
 
+var tempo = { now: null, h72: null, d15: null };
+
+// const connection = mysql.createPool({
+//     connectionLimit: 100,
+//     host: 'localhost',
+//     user: 'admin',
+//     password: '@Unicesumar2018',
+//     database: 'agroamigos',
+//     port: 3306,
+//     debug: false,
+//     multipleStatements: true
+// });
+
 const connection = mysql.createPool({
     connectionLimit: 100,
     host: 'localhost',
-    user: 'admin',
-    password: '@Unicesumar2018',
+    user: 'root',
+    password: 'root',
     database: 'agroamigos',
     port: 3306,
     debug: false,
@@ -126,6 +139,8 @@ app.post('/login', (req, res) => {
                             const refreshToken = jwt.sign(user, config.refreshTokenSecret, { expiresIn: config.refreshTokenLife })
                             const response = {
                                 "id_user": rows[0]["ID_USER"],
+                                "cidade": rows[0]["CIDADE"],
+                                "uf": rows[0]["UF"],
                                 "status": "OK",
                                 "token": token,
                                 "refreshToken": refreshToken,
@@ -229,6 +244,50 @@ app.get('/noticias', function (req, res) {
         console.log(body)
     });
 });
+
+var token1 = '59dfd2d8568a23a0818dbedb70d7c3dc'
+var token2 = '3f0374415e504cd012951020ad5d6bcb'
+app.post('/tempo', function (req, res) {
+    const tempoData = req.body;
+    console.log(tempoData)
+
+    let uf = getSiglaEstado(tempoData.uf)
+    let cidade = tempoData.cidade
+    console.log(uf, cidade)
+    let url = 'http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=' + cidade + '&state=' + uf + '&token=' + token1
+    url = encodeURI(url)
+    request.get(url, function (error, response, body) {
+        var idCidade = JSON.parse(body);
+        idCidade = idCidade[0]['id'];
+        getForecast72(idCidade, res)
+        console.log(idCidade)
+    });
+});
+
+function getForecast72(city, res) {
+    let url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/' + city + '/hours/72?token=' + token1
+    request.get(url, function (error, response, body) {
+        tempo.h72 = JSON.parse(body);
+        getForecastNow(city, res);
+    });
+}
+
+function getForecastNow(city, res) {
+    let url = 'http://apiadvisor.climatempo.com.br/api/v1/weather/locale/' + city + '/current?token=' + token1
+    request.get(url, function (error, response, body) {
+        tempo.now = JSON.parse(body);
+        getForecast15Day(city, res);
+    });
+}
+
+function getForecast15Day(city, res) {
+    let url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/' + city + '/days/15?token=' + token1
+    request.get(url, function (error, response, body) {
+        tempo.d15 = JSON.parse(body);;
+        res.send(tempo)
+    });
+}
+
 
 app.get('/gerar', function (req, res) {
     request.get('http://www.cepea.esalq.usp.br/br/widgetproduto.js.php?fonte=arial&tamanho=10&largura=400px&corfundo=dbd6b2&cortexto=333333&corlinha=ede7bf&id_indicador%5B%5D=50&id_indicador%5B%5D=149&id_indicador%5B%5D=35&id_indicador%5B%5D=143&id_indicador%5B%5D=53&id_indicador%5B%5D=54&id_indicador%5B%5D=91&id_indicador%5B%5D=8&id_indicador%5B%5D=3&id_indicador%5B%5D=2&id_indicador%5B%5D=23&id_indicador%5B%5D=24&id_indicador%5B%5D=162&id_indicador%5B%5D=101&id_indicador%5B%5D=208&id_indicador%5B%5D=76&id_indicador%5B%5D=104&id_indicador%5B%5D=209&id_indicador%5B%5D=119&id_indicador%5B%5D=75&id_indicador%5B%5D=100&id_indicador%5B%5D=103&id_indicador%5B%5D=181&id_indicador%5B%5D=130&id_indicador%5B%5D=leite&id_indicador%5B%5D=72&id_indicador%5B%5D=77&id_indicador%5B%5D=305-BA&id_indicador%5B%5D=305-CE&id_indicador%5B%5D=305-MS&id_indicador%5B%5D=305-MT&id_indicador%5B%5D=305-PR&id_indicador%5B%5D=305-RS&id_indicador%5B%5D=305-SP&id_indicador%5B%5D=159&id_indicador%5B%5D=12&id_indicador%5B%5D=92&id_indicador%5B%5D=129-10&id_indicador%5B%5D=129-6&id_indicador%5B%5D=129-4&id_indicador%5B%5D=129-5&id_indicador%5B%5D=129-1&id_indicador%5B%5D=124&id_indicador%5B%5D=178&id_indicador%5B%5D=179', function (error, response, body) {
@@ -351,8 +410,138 @@ app.get('/cotacoes/user/:id', (req, res) => {
             connection.release();
         }
     });
-
 })
+
+app.get('/config/user/:id', (req, res) => {
+    console.log("cotacoes")
+    const userId = req.params.id;
+    const sql = 'select * from user_config where COD_USER = ?'
+    console.log(sql)
+    const appData = {
+        'error': 1,
+        'data': ''
+    };
+    connection.getConnection(function (err, connection) {
+        if (err) {
+            appData['error'] = err;
+            appData['data'] = 'Internal Server Error';
+            res.status(500).json(appData);
+        } else {
+            connection.query(sql, userId, function (err, rows, fields) {
+                if (!err) {
+                    res.send(rows)
+                } else {
+                    appData['error'] = true;
+                    appData['data'] = err;
+                    res.status(400).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+})
+
+app.delete('/config/user/:id', (req, res) => {
+    console.log("cotacoes")
+    const userId = req.params.id;
+    const sql = 'DELETE FROM user_config where COD_USER = ?'
+    console.log(sql)
+    const appData = {
+        'error': 1,
+        'data': ''
+    };
+    connection.getConnection(function (err, connection) {
+        if (err) {
+            appData['error'] = err;
+            appData['data'] = 'Internal Server Error';
+            res.status(500).json(appData);
+        } else {
+            connection.query(sql, userId, function (err, rows, fields) {
+                if (!err) {
+                    const response = {
+                        "status": "OK",
+                    }
+                    res.status(200).json(response);
+                } else {
+                    appData['error'] = true;
+                    appData['data'] = err;
+                    res.status(400).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+})
+
+app.put('/localizacao/user/', (req, res) => {
+    const putLocalizacao = req.body;
+    const user = {
+        "id": putLocalizacao.id,
+        "uf": putLocalizacao.uf,
+        "cidade": putLocalizacao.cidade
+    }
+    const sql = 'UPDATE user SET UF = "' + user.uf + '", CIDADE = "' + user.cidade + '" WHERE ID_USER = "' + user.id + '"';
+    console.log(sql)
+    const appData = {
+        'error': 1,
+        'data': ''
+    };
+    connection.getConnection(function (err, connection) {
+        if (err) {
+            appData['error'] = err;
+            appData['data'] = 'Internal Server Error';
+            res.status(500).json(appData);
+        } else {
+            connection.query(sql, function (err, rows, fields) {
+                if (!err) {
+                    const response = {
+                        "status": "OK",
+                    }
+                    res.status(200).json(response);
+                } else {
+                    appData['error'] = true;
+                    appData['data'] = err;
+                    res.status(400).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+})
+
+function getSiglaEstado(uf) {
+    var data;
+    switch (uf.toUpperCase()) {
+        case "ACRE": data = "AC"; break;
+        case "ALAGOAS": data = "AL"; break;
+        case "AMAZONAS": data = "AM"; break;
+        case "AMAPÁ": data = "AP"; break;
+        case "BAHIA": data = "BA"; break;
+        case "CEARÁ": data = "CE"; break;
+        case "DISTRITO FEDERAL": data = "DF"; break;
+        case "ESPÍRITO SANTO": data = "ES"; break;
+        case "GOIÁS": data = "GO"; break;
+        case "MARANHÃO": data = "MA"; break;
+        case "MINAS GERAIS": data = "MG"; break;
+        case "MATO GROSSO DO SUL": data = "MS"; break;
+        case "MATO GROSSO": data = "MT"; break;
+        case "PARÁ": data = "PA"; break;
+        case "PARAÍBA": data = "PB"; break;
+        case "PERNAMBUCO": data = "PE"; break;
+        case "PIAUÍ": data = "PI"; break;
+        case "PARANÁ": data = "PR"; break;
+        case "RIO DE JANEIRO": data = "RJ"; break;
+        case "RIO GRANDE DO NORTE": data = "RN"; break;
+        case "RONDÔNIA": data = "RO"; break;
+        case "RORAIMA": data = "RR"; break;
+        case "RIO GRANDE DO SUL": data = "RS"; break;
+        case "SANTA CATARINA": data = "SC"; break;
+        case "SERGIPE": data = "SE"; break;
+        case "SÃO PAULO": data = "SP"; break;
+        case "TOCANTÍNS": data = "TO"; break;
+    }
+    return data;
+};
 
 
 app.listen(3000, function () {
