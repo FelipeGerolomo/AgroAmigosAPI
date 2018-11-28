8ad72cb3196f46883b7a3570f914d2cb406f8a55
DESKTOP-LLU9OKL\FelipeGerolomo <felipegerolomo@gmail.com>
2018-11-27 22:13:37 -0200

    noticias

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var request = require('request').defaults({ rejectUnauthorized: false });
 const config = require('./config')
 moment().format();
 app.use(cors());
+const https = require('https');
 const tokenList = {};
 // process.env.SECRET_KEY = 'agroamigos';
 
@@ -29,7 +30,7 @@ const connection = mysql.createPool({
     multipleStatements: true
 });
 
-gerar();
+//gerar();
 
 cron.schedule("00 17 * * *", function () {
     console.log("---------------------");
@@ -247,11 +248,62 @@ app.get('/config/:user', function (req, res) {
     });
 });
 
+// app.get('/noticias', function (req, res) {
+//     request.get('https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=agronegocio&originalImg=true&freshness=week', { headers: { 'Ocp-Apim-Subscription-Key': 'dfadc1af341143a29cb7cd0c81702def' } }, function (error, response, body) {
+//         res.send(body)
+//         console.log(response)
+//     });
+// });
+
 app.get('/noticias', function (req, res) {
-    request.get('https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=agronegocio&originalImg=true&freshness=week', { headers: { 'Ocp-Apim-Subscription-Key': 'dfadc1af341143a29cb7cd0c81702def' } }, function (error, response, body) {
-        res.send(body)
-        console.log(response)
+    console.log("news")
+    // Replace the subscriptionKey string value with your valid subscription key.
+let subscriptionKey = 'dfadc1af341143a29cb7cd0c81702def';
+
+// Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+// search APIs.  In the future, regional endpoints may be available.  If you
+// encounter unexpected authorization errors, double-check this host against
+// the endpoint for your Bing Search instance in your Azure dashboard.
+let host = 'api.cognitive.microsoft.com';
+let path = '/bing/v7.0/news/search';
+
+let term = 'agronegocio';
+
+let response_handler = function (response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
     });
+    response.on('end', function () {
+        console.log('\nRelevant Headers:\n');
+        for (var header in response.headers)
+            // header keys are lower-cased by Node.js
+            if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+                 console.log(header + ": " + response.headers[header]);
+        body = JSON.stringify(JSON.parse(body), null, '  ');
+        console.log('\nJSON Response:\n');
+        res.send(body)
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+let bing_news_search = function (search) {
+  console.log('Searching news for: ' + term);
+  let request_params = {
+        method : 'GET',
+        hostname : host,
+        path : path + '?q=' + encodeURIComponent(search),
+        headers : {
+            'Ocp-Apim-Subscription-Key' : subscriptionKey,
+        }
+    };
+
+    let req = https.request(request_params, response_handler);
+    req.end();
+}
+bing_news_search(term);
 });
 
 var token1 = '59dfd2d8568a23a0818dbedb70d7c3dc'
